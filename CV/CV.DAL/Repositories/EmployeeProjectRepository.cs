ee39de0223145bff36927435dac551ad80570f9c
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-07-26 20:04:31 +0300

    deleted usings

--- CV/CV.DAL/Repositories/EmployeeProjectRepository.cs
+++ CV/CV.DAL/Repositories/EmployeeProjectRepository.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CV.DAL.EF;
+﻿using CV.DAL.EF;
 using CV.DAL.Entities;
-using CV.DAL.Interfaces;
 
 namespace CV.DAL.Repositories
 {
