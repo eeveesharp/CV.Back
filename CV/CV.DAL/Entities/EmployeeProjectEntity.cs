126808c52e7577e81e8728bc6482d7e39f581302
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-07-26 17:04:27 +0300

    add intermediate table

---
+++ CV/CV.DAL/Entities/EmployeeProjectEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV.DAL.Infraestructure;
+
+namespace CV.DAL.Entities
+{
+    public class EmployeeProjectEntity : HasId
+    {
+        public int EmployeeId { get; set; }
+
+        public int ProjectId { get; set; }
+
+#nullable disable
+        public List<EmployeeEntity> EmployeeList { get; set; }
+
+        public ProjectEntity Project { get; set; }
+#nullable enable
+    }
+}
