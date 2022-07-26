d8d996790918f73b12b50f759796c53662f26db4
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-07-26 19:55:22 +0300

    fixed

--- CV/CV.DAL/Entities/EmployeeProjectEntity.cs
+++ CV/CV.DAL/Entities/EmployeeProjectEntity.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CV.DAL.Infraestructure;
-
-namespace CV.DAL.Entities
+﻿namespace CV.DAL.Entities
 {
-    public class EmployeeProjectEntity 
+    public class EmployeeProjectEntity
     {
         public int Id { get; set; }
 
         public int EmployeeId { get; set; }
 #nullable disable
         public List<EmployeeEntity> Employee { get; set; }
-
+#nullable enable
         public int ProjectId { get; set; }
-
+#nullable disable
         public List<ProjectEntity> Project { get; set; }
 
         public string Position { get; set; }
