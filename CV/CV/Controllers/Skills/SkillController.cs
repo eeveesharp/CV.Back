a8fd8b91adf2b027cdd55c25175925414202bbc5
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-07-27 16:25:53 +0300

    added controllers for skills

---
+++ CV/CV/Controllers/Skills/SkillController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CV.API.ViewModel.SkillViewModels.Skill;
+using CV.BLL.Interfaces;
+using CV.BLL.Models.Skill;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV.API.Controllers.Skills
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillController : GenericController<SkillViewModel, ShortSkillViewModel, AddSkillViewModel, SkillModel, UpdateSkillViewModel>
+    {
+        public SkillController(IGenericServices<SkillModel> genericService, IMapper mapper) : base(genericService, mapper)
+        {
+        }
+    }
+}
---
+++ CV/CV/Controllers/Skills/SkillGroupController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CV.API.ViewModel.SkillViewModels.SkillGroup;
+using CV.BLL.Interfaces;
+using CV.BLL.Models.Skill;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV.API.Controllers.Skills
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillGroupController : GenericController<SkillGroupViewModel, ShortSkillGroupViewModel, AddSkillGroupViewModel, SkillGroupModel, UpdateSkillGroupViewModel>
+    {
+        public SkillGroupController(IGenericServices<SkillGroupModel> genericService, IMapper mapper) : base(genericService, mapper)
+        {
+        }
+    }
+}
